e752afeb9e7b2368b73a14560f8494433a525cd2
Stephen Pham <pham.stephen@gmail.com>
2019-09-30 12:09:25 +1000

    Validate booking time

--- src/booking.js
+++ src/booking.js
@@ -1,3 +1,23 @@
-export default () => {
-  return true
+import moment from 'moment';
+
+export const isValidBooking = (fromTimeString, toTimeString) => {
+  var fromTime = moment(fromTimeString);
+  var toTime = moment(toTimeString)
+
+  var duration = moment.duration(toTime.diff(fromTime)).asHours()
+  if (duration < 1 || duration >= 23) {
+    return false
+  }
+
+  return true;
+}
+
+export const getRateType = (booking) => {
+  var fromTime = moment(booking.from);
+
+  var fromHourOfDay = fromTime.hour();
+  if (fromHourOfDay > 6 && fromHourOfDay < 20) {
+    return 'Day'
+  }
+  return 'Night'
 }
--- src/booking.test.js
+++ src/booking.test.js
@@ -1,7 +1,43 @@
-import booking from './booking'
+import { isValidBooking, getRateType } from './booking'
 
-describe('test', () => {
-  it('return true', () => {
-    expect(booking()).toBeTruthy()
+describe('minimum booking time', () => {
+  it('should be a valid booking time', () => {
+    var isValid = isValidBooking('2017-10-23T08:00:00+11:00', '2017-10-23T11:00:00+11:00');
+    expect(isValid).toBeTruthy();
+  })
+
+  it('should be an invalid booking time when time is the same', () => {
+    var isValid = isValidBooking('2017-10-23T08:00:00+11:00', '2017-10-23T08:00:00+11:00');
+    expect(isValid).toBeFalsy();
+  })
+
+  it('should be an invalid booking time when time difference is less than 1 hr', () => {
+    var isValid = isValidBooking('2017-10-23T08:00:00+11:00', '2017-10-23T08:30:00+11:00');
+    expect(isValid).toBeFalsy();
+  })
+
+  it('should be an invalid booking time is over 24hrs', () => {
+    var isValid = isValidBooking('2017-05-23T08:00:00+11:00', '2017-05-24T08:00:00+11:00');
+    expect(isValid).toBeFalsy();
+  })
+})
+
+describe('rate type', () => {
+  it('should be a day rate', () => {
+    var booking = {
+      from: "2017-10-23T08:00:00+11:00",
+      to: "2017-10-23T11:00:00+11:00"
+    }
+    var rateType = getRateType(booking);
+    expect(rateType).toEqual('Day');
+  })
+
+  it('should be night rate', () => {
+    var booking = {
+      from: "2017-10-23T20:00:00+11:00",
+      to: "2017-10-24T11:02:00+11:00"
+    }
+    var rateType = getRateType(booking);
+    expect(rateType).toEqual('Night');
   })
 })
